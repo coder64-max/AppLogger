e5eee336b71e47dfdf3c833defb6e6eb4973753c
Jarkko <jarkkopikk@gmail.com>
2024-11-03 16:04:59 +0200

    Initial commit of AppLogger project

---
+++ App/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using AppLogger;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.Log("Hello from App!");
+    }
+}
+// See https://aka.ms/new-console-template for more information
